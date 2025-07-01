6b40909bdcaad4afe70f2bac475c3d6b8e088c6c
Jenn López G. <jenn0106_log@outlook.com>
2025-07-01 09:32:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,20 @@
 from flask import Flask, render_template, request
-import google.generativeai as genai
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
+from ia import responder_con_ia
 
 app = Flask(__name__)
-
-genai.configure(api_key=os.getenv("API_KEY"))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     respuesta_ia = ""
     pregunta = ""
+    modelo = ""
+    fragmentos = []
+    
     if request.method == "POST":
-        pregunta = request.form["consulta"]
-        respuesta_ia = responder_con_ia(pregunta)
-    return render_template("index.html", respuesta=respuesta_ia, pregunta=pregunta)
-
-def responder_con_ia(pregunta):
-    model = genai.GenerativeModel("gemini-pro")
-    respuesta = model.generate_content(pregunta)
-    return respuesta.text
+        pregunta = request.form.get("consulta", "")
+        respuesta_ia, modelo, fragmentos = responder_con_ia(pregunta)
+        
+    return render_template("index.html", respuesta=respuesta_ia, pregunta=pregunta, modelo=modelo, fragmentos=fragmentos)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
